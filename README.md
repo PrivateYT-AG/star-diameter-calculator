20858a9c75d68436e98b223e4ea00c205e0c3469
PrivateYT-AG <privateu691@gmail.com>
2026-01-25 06:27:58 +0700

    Added core files

---
+++ README.md
@@ -0,0 +1,19 @@
+# Star Diameter Calculator
+A simple web-based tool to measure a star's diameter, from its radius in solar radii (R☉).
+
+## What it does
+- Takes a star's **radius** (in solar radii, R☉)
+- Calculator converts it to kilometers, doubles it, giving the **diameter** 
+
+## How it works
+It uses a simple formula:
+
+$Diameter = radius \times 2$
+
+You just have to input the star's radius, and press "Calculate diameter", and it will show you the result
+
+## Features
+- A simple UI
+
+## License
+This project was licensed under the MIT license. See [here](LICENSE) for more details.
---
+++ assets/js/script.js
@@ -0,0 +1,107 @@
+const SOLAR_RADIUS = 695700; // in km
+const MAX_INPUT_VALUE = 10000;
+const overlay = document.getElementById("overlay");
+
+function clearResult() {
+  document.getElementById("result").textContent = "";
+  document.getElementById("error").textContent = "";
+  document.getElementById("star-input").value = "";
+}
+
+function formatNumber(num) {
+  return num.toLocaleString(navigator.language, { 
+    minFractionDigits: 0, 
+    maxFractionDigits: 3
+  });
+}
+
+function openWindow(id) {
+  const win = document.getElementById(id);
+  if (!win) return;
+
+  win.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+function closeWindow(win) {
+  if (!win) return;
+
+  win.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+function validateNumberInput(inputId,errorId) {
+  const input = document.getElementById(inputId).value.trim();
+  const numberRegex = /^\d+(\.\d+)?$/;
+  const numberValue = Number(input);
+
+  if (!numberRegex.test(input)) {
+    document.getElementById(errorId).textContent = "Invalid input. Please enter a valid number.";
+    console.error("Invalid input. Please enter a valid number.");
+    return null;
+  } else if (numberValue <= 0) {
+    document.getElementById(errorId).textContent = "Value must be greater than zero.";
+    console.error("Value must be greater than zero.");
+    return null;
+  } else if (numberValue > MAX_INPUT_VALUE) {
+    document.getElementById(errorId).textContent = "Input value cannot be more than " + MAX_INPUT_VALUE + ".";
+    console.error("Input value cannot be more than " + MAX_INPUT_VALUE + ".");
+    return null;
+  }
+  document.getElementById(errorId).textContent = "";
+  return numberValue;
+}
+
+function getStarDiameter() {
+  const starRadius = validateNumberInput("star-input","error");
+  if (starRadius === null) {
+    document.getElementById("result").textContent = "";
+    return;
+  }
+
+  const diameter = starRadius * SOLAR_RADIUS * 2;
+  console.log("Diameter:", formatNumber(diameter), "km");
+  document.getElementById("result").textContent = "Diameter: " + formatNumber(diameter) + " km";
+}
+
+// keyboard shortcuts, etc
+document.addEventListener("keydown", function(event) {
+  const key = event.key;
+  if (key === "Enter") {
+    getStarDiameter()
+  } else if (event.ctrlKey && key === "Backspace") {
+    clearResult()
+  } else if (key === "Escape") {
+    document.querySelectorAll(".popup:not(.hidden)").forEach(win => {
+      closeWindow(win);
+    });
+    clearResult()
+  }
+});
+
+document.querySelectorAll(".view-link").forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+
+    const targetId = link.dataset.target;
+    const target = document.getElementById(targetId);
+
+    if (target) {
+      target.classList.remove("hidden");
+      overlay.classList.remove("hidden");
+    }
+  });
+});
+
+document.querySelectorAll(".popup .close-x").forEach(button => {
+  button.addEventListener("click", e => {
+    e.preventDefault();
+    closeWindow(button.closest(".popup"));
+  });
+});
+
+overlay.addEventListener("click", () => {
+  document.querySelectorAll(".popup:not(.hidden)").forEach(win => {
+    closeWindow(win);
+  });
+});
