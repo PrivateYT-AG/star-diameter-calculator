35ab768ce448d707b010894b20c8686c93b2c70b
PrivateYT <privateu691@gmail.com>
2026-01-26 21:40:02 +0700

    Updated theme-switch

--- assets/js/theme-switch.js
+++ assets/js/theme-switch.js
@@ -1,17 +1,53 @@
 const lightBtn = document.getElementById("light-mode-btn");
 const darkBtn = document.getElementById("dark-mode-btn");
+const systemBtn = document.getElementById("system-theme-btn");
+
+const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+function applyTheme(theme) {
+  document.body.classList.remove("dark-mode");
+
+  if (theme === "dark") {
+    document.body.classList.add("dark-mode");
+  } else if (theme === "system") {
+    if (mediaQuery.matches) {
+      document.body.classList.add("dark-mode")
+    }
+  }
+}
 
 lightBtn.addEventListener("click", () => {
-  document.body.classList.remove("dark-mode");
   localStorage.setItem("theme", "light");
+  applyTheme("light");
+  updateActive();
 });
 
 darkBtn.addEventListener("click", () => {
-  document.body.classList.add("dark-mode");
   localStorage.setItem("theme", "dark");
+  applyTheme("dark");
+  updateActive();
 });
 
-const savedTheme = localStorage.getItem("theme");
-if (savedTheme === "dark") {
-  document.classList.add("dark-mode");
+systemBtn.addEventListener("click", () => {
+  localStorage.setItem("theme", "system");
+  applyTheme("system");
+  updateActive();
+});
+
+mediaQuery.addEventListener("change", () => {
+  if (localStorage.getItem("theme") === "system") {
+    applyTheme("system");
+  }
+});
+
+function updateActive() {
+  const theme = localStorage.getItem("theme") || "system";
+
+  lightBtn.classList.toggle("active", theme === "light");
+  darkBtn.classList.toggle("active", theme === "dark");
+  systemBtn.classList.toggle("active", theme === "system");
 }
+
+const savedTheme = localStorage.getItem("theme") || "system";
+applyTheme(savedTheme);
+updateActive();
